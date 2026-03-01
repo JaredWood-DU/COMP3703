4f7303b11d4c74a5154bfd743ead2a4f5fbfceef
JaredWood-DU <Jared.Wood@du.edu>
2026-03-01 13:28:39 -0700

    Added basic templates in preparation for data exploration

---
+++ final/explore_normal_data.py
@@ -0,0 +1,72 @@
+'''
+FILE OVERVIEW:
+- 
+
+=================================================
+
+MISC COMMENTS:
+- 
+
+=================================================
+
+FILE CONTENTS:
+- File Overview, Imports, Global Variables
+- Helper Functions
+    - Thing 1
+    - Thing 2
+- Main Function
+'''
+# ----- Imports -----------------------------------------------------------------------------------
+
+
+# ----- Global Variables --------------------------------------------------------------------------
+
+
+# =================================================================================================
+# END File Overview, Imports, Global Variables
+# START Helper Functions
+# =================================================================================================
+
+def thing1():
+    '''
+    About
+    -----
+    - Some placeholder function
+
+    Parameters
+    ----------
+    - ray_nn_train_func (Function) :
+        - The Ray-Train function logic for MLflow to wrap and log information from
+
+    - framework (str) :
+        - Default: pytorch (Not implemented)
+        - String representation of the NN framework used (NOT IMPLEMENTED)
+
+    Raises
+    ------
+    - RunTimeError
+        - Generally if anything should fail to log properly
+
+    - NotImplementedError
+        - Generally if something has not been implemented yet, particularly with framework types
+
+    Returns
+    -------
+    - Wraps the Ray-train function with MLflow logging logic to display results on MLflow UI
+    '''
+    pass
+
+
+def thing2():
+    pass
+
+# =================================================================================================
+# END Helper Functions
+# START Main Function
+# =================================================================================================
+
+
+
+# =================================================================================================
+# END Main Function
+# =================================================================================================
