4e84ef1d78001077930e02ebb7c3b45ae0c06b59
JaredWood-DU <Jared.Wood@du.edu>
2026-03-10 11:57:32 -0600

    General template created

---
+++ final/final.py
@@ -0,0 +1,74 @@
+'''
+FILE OVERVIEW:
+- Underlying code intended for use in final.ipynb to keep notebook cleaner
+- Generally will use visualizations and functions produced from other .py files, so anything here is because
+  it is specific for this .ipynb
+
+=================================================
+
+MISC COMMENTS:
+- NA
+
+=================================================
+
+FILE CONTENTS:
+- File Overview, Imports, Global Variables
+- Helper Functions
+    - Thing 1
+    - Thing 2
+- Main Function
+'''
+# ----- Imports -----------------------------------------------------------------------------------
+
+
+# ----- Global Variables --------------------------------------------------------------------------
+# NA
+
+# =================================================================================================
+# END File Overview, Imports, Global Variables
+# START Helper Functions
+# =================================================================================================
+
+def thing1():
+    '''
+    About
+    -----
+    - Some placeholder function
+
+    Parameters
+    ----------
+    - ray_nn_train_func (Function) :
+        - The Ray-Train function logic for MLflow to wrap and log information from
+
+    - framework (str) :
+        - Default: pytorch (Not implemented)
+        - String representation of the NN framework used (NOT IMPLEMENTED)
+
+    Raises
+    ------
+    - RunTimeError
+        - Generally if anything should fail to log properly
+
+    - NotImplementedError
+        - Generally if something has not been implemented yet, particularly with framework types
+
+    Returns
+    -------
+    - Wraps the Ray-train function with MLflow logging logic to display results on MLflow UI
+    '''
+    pass
+
+
+def thing2():
+    pass
+
+# =================================================================================================
+# END Helper Functions
+# START Main Function
+# =================================================================================================
+
+
+
+# =================================================================================================
+# END Main Function
+# =================================================================================================
